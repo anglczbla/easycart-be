d539cc183e83ed6a51894081f38a1c767321ce03
anglczbla <angelicaizabella90@gmail.com>
2026-03-24 14:07:12 +0700

    feat: implement to routes cart

--- routes/cart.ts
+++ routes/cart.ts
@@ -2,11 +2,22 @@ import express from "express";
 import cartController from "../controllers/cartController.ts";
 import authUser from "../middleware/auth.ts";
 
+import {
+  validateAddToCart,
+  validateDeleteCart,
+  validateUpdateCartQty,
+} from "../middleware/validation/cartValidation.ts";
+
 const router = express.Router();
 
 router.get("/", authUser, cartController.getCartById);
-router.post("/", authUser, cartController.addToCart);
-router.put("/:id", authUser, cartController.updateCartQty);
-router.delete("/:id", authUser, cartController.deleteCart);
+router.post("/", authUser, validateAddToCart, cartController.addToCart);
+router.put(
+  "/:id",
+  authUser,
+  validateUpdateCartQty,
+  cartController.updateCartQty,
+);
+router.delete("/:id", authUser, validateDeleteCart, cartController.deleteCart);
 
 export default router;
